94b59506b2207c37a77ba74e0e72f4e8fd047f05
unknown <sosnovskaaanuta@gmail.com>
2025-06-22 02:25:55 +0500

    commit551

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-set -e
+alembic upgrade head
 
-# Запускаем миграции с правильным путем к ini (в src)
-alembic -c ./alembic.ini upgrade head
+cd src 
 
-# Запускаем приложение (main.py в src)
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+
