5f3468ee689c1f56684d49a32bb078e74222e0b8
unknown <sosnovskaaanuta@gmail.com>
2025-06-22 00:45:51 +0500

    commit55151

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Прогоняем миграции с явным указанием конфига
+# Выход при ошибке любой команды
+set -e
+
+# Прогоняем миграции
 alembic -c alembic.ini upgrade head
 
-# Запускаем gunicorn с правильным модулем
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+# Запускаем сервер
+gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
