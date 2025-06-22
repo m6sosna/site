8d0f5afec5a781d9e55096ae85b841fa00402e44
unknown <sosnovskaaanuta@gmail.com>
2025-06-22 16:21:49 +0500

    commit551

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -3,6 +3,9 @@ set -e
 
 cd backend/src
 
+while ! nc -z db 5432; do
+  sleep 1
+done
 alembic upgrade head
 
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
