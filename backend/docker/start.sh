6ad17c5b1926fe009b7f1fb1b51e24936125d72f
unknown <sosnovskaaanuta@gmail.com>
2025-06-22 16:35:01 +0500

    commit551

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -3,9 +3,14 @@ set -e
 
 cd backend/src
 
-while ! nc -z db 5432; do
+echo "Waiting for database $DB_HOST:$DB_PORT..."
+
+while ! nc -z $DB_HOST $DB_PORT; do
   sleep 1
 done
+
+echo "Database is up, running migrations..."
 alembic upgrade head
 
+echo "Starting app..."
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
